18185bb45b90d75cd55043c01c8d47f2cb6032cf
Yavuz SAV <yavuzsav07@gmail.com>
2020-03-30 23:37:11 +0300

    End of Section 9

--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -12,7 +12,7 @@ namespace DatingApp.API.Dtos
         public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
-        public DateTime LastActivite { get; set; }
+        public DateTime LastActive { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
