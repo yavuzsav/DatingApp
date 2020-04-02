319996742ca1a604674db22f90bc674b2ecc6df5
Yavuz SAV <yavuzsav07@gmail.com>
2020-04-02 22:13:59 +0300

    End of Section 12

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -26,6 +26,8 @@ namespace DatingApp.API.Helpers
             CreateMap<Photo, PhotoForReturnDto>();
 
             CreateMap<PhotoForCreationDto, Photo>();
+
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
